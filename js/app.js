bc18135f3ed1c5363f62699bdb3c584713badd6a
Joevanni Thompson <JoevanniTho@hotmail.com>
2020-05-12 21:11:57 -0400

    Added transformation
    Rotates the cells 180 degrees and turns them back ONLY when the the user clicks a different cell.

---
+++ js/app.js
@@ -0,0 +1,102 @@
+let table = document.querySelectorAll(`td`);
+
+table[0].addEventListener(`click`, () => {
+    table[0].style.transform = `rotate(180deg)`;
+    table[0].style.animation = `turn`;
+    table[0].style.animationDuration = `.5s`;
+    table[0].style.color = `#55bada`;
+    if(table[1].style.transform === `rotate(180deg)`){
+        table[1].style.transform = `rotate(0deg)`;
+        table[1].style.animation = `return`;
+        table[1].style.animationDuration = `.5s`;
+        table[1].style.color = `black`;
+    }
+    else if(table[2].style.transform === `rotate(180deg)`){
+        table[2].style.transform = `rotate(0deg)`;
+        table[2].style.animation = `return`;
+        table[2].style.animationDuration = `.5s`;
+        table[2].style.color = `black`;
+    }
+    else if(table[3].style.transform === `rotate(180deg)`){
+        table[3].style.transform = `rotate(0deg)`;
+        table[3].style.animation = `return`;
+        table[3].style.animationDuration = `.5s`;
+        table[3].style.color = `black`;
+    }
+});
+
+table[1].addEventListener(`click`, () => {
+    table[1].style.transform = `rotate(180deg)`;
+    table[1].style.animation = `turn`;
+    table[1].style.animationDuration = `.5s`;
+    table[1].style.color = `#55bada`;
+    if(table[0].style.transform === `rotate(180deg)`){
+        table[0].style.transform = `rotate(0deg)`;
+        table[0].style.animation = `return`;
+        table[0].style.animationDuration = `.5s`;
+        table[0].style.color = `black`;
+    }
+    else if(table[2].style.transform === `rotate(180deg)`){
+        table[2].style.transform = `rotate(0deg)`;
+        table[2].style.animation = `return`;
+        table[2].style.animationDuration = `.5s`;
+        table[2].style.color = `black`;
+    }
+    else if(table[3].style.transform === `rotate(180deg)`){
+        table[3].style.transform = `rotate(0deg)`;
+        table[3].style.animation = `return`;
+        table[3].style.animationDuration = `.5s`;
+        table[3].style.color = `black`;
+    }
+});
+
+table[2].addEventListener(`click`, () => {
+    table[2].style.transform = `rotate(180deg)`;
+    table[2].style.animation = `turn`;
+    table[2].style.animationDuration = `.5s`;
+    table[2].style.color = `#55bada`;
+    if(table[0].style.transform === `rotate(180deg)`){
+        table[0].style.transform = `rotate(0deg)`;
+        table[0].style.animation = `return`;
+        table[0].style.animationDuration = `.5s`;
+        table[0].style.color = `black`;
+    }
+    else if(table[1].style.transform === `rotate(180deg)`){
+        table[1].style.transform = `rotate(0deg)`;
+        table[1].style.animation = `return`;
+        table[1].style.animationDuration = `.5s`;
+        table[1].style.color = `black`;
+    }
+    else if(table[3].style.transform === `rotate(180deg)`){
+        table[3].style.transform = `rotate(0deg)`;
+        table[3].style.animation = `return`;
+        table[3].style.animationDuration = `.5s`;
+        table[3].style.color = `black`;
+    }
+});
+
+table[3].addEventListener(`click`, () => {
+    table[3].style.transform = `rotate(180deg)`;
+    table[3].style.animation = `turn`;
+    table[3].style.animationDuration = `.5s`;
+    table[3].style.color = `#55bada`;
+    if(table[0].style.transform === `rotate(180deg)`){
+        table[0].style.transform = `rotate(0deg)`;
+        table[0].style.animation = `return`;
+        table[0].style.animationDuration = `.5s`;
+        table[0].style.color = `black`;
+    }
+    else if(table[1].style.transform === `rotate(180deg)`){
+        table[1].style.transform = `rotate(0deg)`;
+        table[1].style.animation = `return`;
+        table[1].style.animationDuration = `.5s`;
+        table[1].style.color = `black`;
+    }
+    else if(table[2].style.transform === `rotate(180deg)`){
+        table[2].style.transform = `rotate(0deg)`;
+        table[2].style.animation = `return`;
+        table[2].style.animationDuration = `.5s`;
+        table[2].style.color = `black`;
+    }
+});
+
